714f2858d37101d56be7fa3589e7e595990aa2cb
= <alison-cruz@hotmail.com>
2020-05-20 05:09:23 -0300

    Status fixed

--- src/server.js
+++ src/server.js
@@ -1,7 +1,3 @@
-require('dotenv').config();
-
 const app = require("./app");
 
-app.listen(process.env.PORT || 4000, () => {
-  console.log(`Listening on port: ${process.env.PORT}`)
-})
+app.listen(3333);
